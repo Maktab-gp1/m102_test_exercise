826fea7ca44c977788c33a875a327a7fc3d049e8
ImanSMD <110537853+ImanSMD@users.noreply.github.com>
2023-11-10 11:18:04 +0330

    func :test_mean added

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -12,13 +12,24 @@ class TestShoppingCart(unittest.TestCase):
     
     def test_add_item(self):
         self.obj.add_item('ab',6000)
+        self.obj.add_item('mive',6000)
         self.assertGreater(self.obj.item_count , 0 , 'item not added')
     
     def test_remove_item(self):
-        self.obj.remove_item('ab')
-        self.assertEqual(self.obj.item_count,0,'item not removed')
+        self.obj.add_item('ab',6000)
+        self.obj.add_item('mive',6000)
+        print(self.obj.remove_item('ab'))
+        self.assertEqual(self.obj.item_count,1,'item not removed')
     
-    # def 
+    def test_mean_item_price(self):
+        self.obj.add_item('ab',4000)
+        self.obj.add_item('mive',6000)
+        self.assertEqual(self.obj.total/self.obj.item_count,5000,'mean is not calculated')
+    
+    
+
+        
+
 
     
 
