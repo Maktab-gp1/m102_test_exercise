e4747cdc18ce2199a578acf0b6f6a4708025066a
ImanSMD <110537853+ImanSMD@users.noreply.github.com>
2023-11-10 10:25:31 +0330

    test add item added

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,3 +1,20 @@
 from shopping_cart import ShoppingCart
+import unittest
 
 # write your tests here
+first = ShoppingCart
+class TestShoppingCart(unittest.TestCase):
+    def __init__(self):
+        self.obj = ShoppingCart()
+    # def test_setup(self):
+    #     self.first = ShoppingCart()
+    
+    def test_add_item(self):
+        self.obj.add_item('ab',6000)
+        self.assertGrater(first.item_count , 0 , 'item not added')
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+    
